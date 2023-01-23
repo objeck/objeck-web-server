d407ed0a5b74820bc680825a442545f29d75e526
Randy Hollines <objeck@gmail.com>
2023-01-23 09:19:52 -0800

    refreshing code

--- build.cmd
+++ build.cmd
@@ -1,2 +1,14 @@
-obc -src ssl_server.obs -lib net,json
-obr ssl_server keys\cert.crt keys\cert.key 1234 60013
+@echo off
+
+if [%1] == [] goto end
+	obc -src %1\core\compiler\lib_src\net.obs -tar lib -dest %1\core\release\deploy64\lib\net.obl
+	obc -src ssl_server.obs -lib net,json
+
+	if [%2] NEQ [brun] goto brun
+		obr ssl_server keys\cert.crt keys\cert.key 1234 60013
+	:brun
+:end
+
+if [%1] NEQ []  goto usage
+	@echo build.cmd [objeck_dir]
+:usage
