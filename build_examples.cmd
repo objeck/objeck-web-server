d13ff9627fe42466900ffab81757cbda7dc87717
Randy Hollines <objeck@gmail.com>
2023-02-05 14:02:09 -0800

    cleaning up repo

--- build_examples.cmd
+++ build_examples.cmd
@@ -5,6 +5,6 @@ if [%1] NEQ [] (
 	obc -src %1\core\compiler\lib_src\net_common.obs,%1\core\compiler\lib_src\net.obs,%1\core\compiler\lib_src\net_secure.obs -tar lib -dest %1\core\release\deploy64\lib\net.obl
 )
 
-obc -src hello_http.obs -lib net,json
-obc -src form_post_https.obs -lib net,json
-obc -src compress_response_https.obs -lib net,json
+obc -src examples\hello_http.obs -lib net,json
+obc -src examples\form_post_https.obs -lib net,json
+obc -src examples\compress_response_https.obs -lib net,json
