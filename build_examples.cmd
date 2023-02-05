2f28c2e0ec31409f77596a827f1b24fc927084b1
Randy Hollines <objeck@gmail.com>
2023-02-05 13:27:12 -0800

    updating code samples

--- build_examples.cmd
+++ build_examples.cmd
@@ -1,8 +1,10 @@
 @echo off
 
+rem rebuild network libraries
 if [%1] NEQ [] (
 	obc -src %1\core\compiler\lib_src\net_common.obs,%1\core\compiler\lib_src\net.obs,%1\core\compiler\lib_src\net_secure.obs -tar lib -dest %1\core\release\deploy64\lib\net.obl
 )
 
 obc -src hello_http.obs -lib net,json
 obc -src form_post_https.obs -lib net,json
+obc -src compress_response_https.obs -lib net,json
