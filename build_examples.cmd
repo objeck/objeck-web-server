d8f9364aa137d3c86fee6b46a9ed8d188fb6fdff
Randy Hollines <objeck@gmail.com>
2023-02-06 08:13:56 -0800

    webserver file support

--- build_examples.cmd
+++ build_examples.cmd
@@ -7,7 +7,12 @@ if [%1] NEQ [] (
 
 del /q examples\*.obe examples\*.jpg 
 
-obc -src examples\hello_http.obs -lib net,json
-obc -src examples\form_post_https.obs -lib net,json
-obc -src examples\compress_response_https.obs -lib net,json
-obc -src examples\multi_mime_http.obs -lib net,json
+obc -src examples\simple_config_https.obs -lib net,json
+REM obc -src examples\hello_http.obs -lib net,json
+REM obc -src examples\form_post_https.obs -lib net,json
+REM obc -src examples\compress_response_https.obs -lib net,json
+REM obc -src examples\multi_mime_http.obs -lib net,json
+
+if [%2] == [brun] (
+	obr examples\simple_config_https
+)
--- config/build_test.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-pushd .. && build_examples ..\objeck-lang && popd
-obc config_test.obs -lib json,net
