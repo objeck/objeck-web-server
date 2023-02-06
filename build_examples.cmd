76bca7f6e25e88e0e9952c2935b4a75154626019
Randy Hollines <objeck@gmail.com>
2023-02-06 04:12:29 -0800

    web server api configuration

--- build_examples.cmd
+++ build_examples.cmd
@@ -2,7 +2,7 @@
 
 rem rebuild network libraries
 if [%1] NEQ [] (
-	obc -src %1\core\compiler\lib_src\net_common.obs,%1\core\compiler\lib_src\net.obs,%1\core\compiler\lib_src\net_secure.obs -tar lib -dest %1\core\release\deploy64\lib\net.obl
+	obc -src %1\core\compiler\lib_src\net_common.obs,%1\core\compiler\lib_src\net.obs,%1\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %1\core\release\deploy64\lib\net.obl
 )
 
 del /q examples\*.obe examples\*.jpg 
