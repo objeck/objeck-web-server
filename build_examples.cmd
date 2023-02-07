966e78adde154c2f3d90f076f07e6ba2d2279fbe
Randy Hollines <objeck@gmail.com>
2023-02-07 10:06:41 -0800

    password auth example

--- build_examples.cmd
+++ build_examples.cmd
@@ -11,8 +11,9 @@ obc -src examples\simple_config_https.obs -lib net,json
 obc -src examples\hello_http.obs -lib net,json
 obc -src examples\form_post_https.obs -lib net,json
 obc -src examples\compress_response_https.obs -lib net,json
-obc -src examples\multi_mime_http.obs -lib net,json
+obc -src examples\multi_mime_https.obs -lib net,json
+obc -src examples\passwd_https.obs -lib net,json
 
 if [%2] == [brun] (
-	obr examples\multi_mime_http
+	obr examples\passwd_https
 )
