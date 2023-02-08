1e64d0cf6ba487f4e7945283cee8840138d18aa3
Randy Hollines <objeck@gmail.com>
2023-02-07 16:29:27 -0800

    code clean up

---
+++ key_gen/keygen_macos.sh
@@ -0,0 +1,6 @@
+mkdir keys
+cd keys
+openssl genrsa -des3 -out cert.key 2048
+openssl req -x509 -new -nodes -key cert.key -sha256 -days 1825 -out cert.pem  -subj "/CN=localhost"
+sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" cert.pem
+cd ..
